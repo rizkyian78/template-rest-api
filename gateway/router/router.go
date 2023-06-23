42d4c2e7873074df2e526b3a7c9d17fee8e272f2
Rizky Indiarto <rizky.indiarto@brank.as>
2023-06-23 21:21:50 +0700

    fix: add 404 render html and add workspace

--- gateway/router/router.go
+++ gateway/router/router.go
@@ -10,13 +10,15 @@ func Router(h *controller.Handler) *gin.Engine {
 	router := gin.New()
 	router.Use(gin.LoggerWithFormatter(utils.LoggerRequest))
 	router.Use(gin.Recovery())
-	router.LoadHTMLGlob("notfound")
-
-	router.GET("/ping", h.Ping)
-	router.POST("/auth/token", h.OauthToken)
+	router.Static("/css", "../view/style.css")
+	router.LoadHTMLGlob("view/*")
 
 	router.NoRoute(func(ctx *gin.Context) {
 		ctx.HTML(404, "notfound.html", gin.H{})
 	})
+
+	router.GET("/ping", h.Ping)
+	router.POST("/auth/token", h.OauthToken)
+
 	return router
 }
